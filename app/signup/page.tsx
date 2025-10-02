4266dd781a2647480d10c942873ff0b0612097e3
mrmemo87-cell <mr.memo87@gmail.com>
2025-10-02 17:03:49 +0600

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+export const dynamic = 'force-dynamic';
 import { supabase } from '@/lib/supabaseClient';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
