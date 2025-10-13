05e396d639477dd7a7496392c6d96dab6c49f6e1
mrmemo87-cell <mr.memo87@gmail.com>
2025-10-13 14:53:02 +0600

    deploy: apply safe updates in repo root

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,1 @@
-﻿export default function Home() {
-  return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold">Brain Heist</h1>
-      <div className="p-2 text-xs">build: deployed</div>
-    </main>
-  );
-}
-
-
-// deploy-touch 2025-10-13 14:36:14
+// deploy-touch 2025-10-13T14:53:01
